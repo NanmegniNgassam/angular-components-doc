71af9f35be00fa568af6e37d24a240c67920ca9f
Gilles NGASSAM <nanmegningassam@gmail.com>
2026-03-22 19:41:16 +0100

    dev: starting content projection part

---
+++ src/app/feature-card/feature-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'psl-feature-card',
+  imports: [],
+  templateUrl: './feature-card.component.html',
+  styleUrl: './feature-card.component.scss',
+})
+export class FeatureCardComponent {}
